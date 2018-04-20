2478c4496eab9eace0d1a55ae511218452c4e909
ngocan.pham@ogilvy.com <ngocan.pham@ogilvy.com>
2018-04-20 16:29:07 +0700

    NavMesh

---
+++ NavMesh/Assets/Scripts/RotateNavMesh.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateNavMesh : MonoBehaviour {
+	public float m_rotationSpeed = 15.0f;
+	private void Update(){
+		transform.Rotate(Vector3.up * (Time.deltaTime * m_rotationSpeed), Space.World);
+	}
+}
